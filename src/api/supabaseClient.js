7c325dbdacbd3742b281c06f6d54ef4334849c9c
nhanvoks2003 <45304292+nhanvoks2003@users.noreply.github.com>
2026-09-23 05:49:00 +0700

    Config: Connect to new Supabase project

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://dfztdnegyzqolmbsseup.supabase.co';
+const supabaseAnonKey = 'sb_publishable_MTbNpHmfgPjGwLxdgR1ytA_S2siqOa0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
